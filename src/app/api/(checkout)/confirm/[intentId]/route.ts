3e30ac0d8fd1798663040ebeb312f5a533e6ecbc
JerryTohHS <jerrytoh93@gmail.com>
2023-10-28 11:40:31 +0800

    changed checkout>confirm>route for deploy

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,25 +1,53 @@
+// import prisma from "@/utils/connect";
+// import { NextResponse } from "next/server";
+
+// export const PUT = async ({ params }: { params: { intentId: string } }) => {
+//   const { intentId } = params;
+
+//   try {
+//     await prisma.order.update({
+//       where: {
+//         intent_id: intentId,
+//       },
+//       data: { status: "Being prepared!" },
+//     });
+//     return new NextResponse(
+//       JSON.stringify({ message: "Order has been updated" }),
+//       { status: 200 }
+//     );
+//   } catch (err) {
+//     console.log(err);
+//     return new NextResponse(
+//       JSON.stringify({ message: "Something went wrong!" }),
+//       { status: 500 }
+//     );
+//   }
+// };
+
 import prisma from "@/utils/connect";
-import { NextResponse } from "next/server";
+import { NextApiResponse, NextApiRequest } from "next";
 
-export const PUT = async ({ params }: { params: { intentId: string } }) => {
-  const { intentId } = params;
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "PUT") {
+    const { intentId } = req.query;
 
-  try {
-    await prisma.order.update({
-      where: {
-        intent_id: intentId,
-      },
-      data: { status: "Being prepared!" },
-    });
-    return new NextResponse(
-      JSON.stringify({ message: "Order has been updated" }),
-      { status: 200 }
-    );
-  } catch (err) {
-    console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }),
-      { status: 500 }
-    );
+    if (typeof intentId === "string") {
+      try {
+        await prisma.order.update({
+          where: {
+            intent_id: intentId,
+          },
+          data: { status: "Being prepared!" },
+        });
+        res.status(200).json({ message: "Order has been updated" });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Something went wrong!" });
+      }
+    } else {
+      res.status(400).json({ message: "intentId must be a string" });
+    }
+  } else {
+    res.status(405).end(); // Method Not Allowed
   }
 };
