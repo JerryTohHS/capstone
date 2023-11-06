143932cf062e19bbc40bf44e2bc8404960a1e57f
JerryTohHS <jerrytoh93@gmail.com>
2023-11-06 21:18:01 +0800

    edited categories route.ts

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -2,16 +2,21 @@ import cors from "cors";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-// Initialize CORS middleware
-const corsMiddleware = cors({
+// Define your CORS options
+const corsOptions = {
   origin: "https://capstone-mamak.vercel.app", // Replace with your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE"],
   optionsSuccessStatus: 204,
-});
-// FETCH ALL CATEGORIES
+};
+
+// Initialize the CORS middleware with the specified options
+const corsMiddleware = cors(corsOptions);
+
 export const GET = async () => {
-  corsMiddleware;
   try {
+    // Apply the CORS middleware here
+    corsMiddleware;
+
     const categories = await prisma.category.findMany();
     return new NextResponse(JSON.stringify(categories), { status: 200 });
   } catch (err) {
@@ -22,7 +27,19 @@ export const GET = async () => {
     );
   }
 };
+
 export const POST = () => {
-  corsMiddleware;
-  return new NextResponse("Hello", { status: 200 });
+  try {
+    // Apply the CORS middleware here
+    corsMiddleware;
+
+    // Handle POST request logic here
+    return new NextResponse("Hello", { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
 };
